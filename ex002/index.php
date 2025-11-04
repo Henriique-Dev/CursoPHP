473c367fb1df9927632d2ebcb91ec6dd697eceaa
Henriique-Dev <vitorhenriquexaviier@gmail.com>
2025-11-04 09:53:39 -0300

    T

--- ex002/index.php
+++ ex002/index.php
@@ -11,7 +11,7 @@
     <?php 
         date_default_timezone_set("America/Sao_paulo"); // GMT-3
         echo "Hoje é dia " . date("d/M/Y");
-        echo "e a hora atual é " . date("G:i:s");
+        echo "e a hora atual é " . date("G:i:s T");
     ?>
 </body>
 </html>
