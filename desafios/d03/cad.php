d8b5ae0bec42ad0bd3f18d417e281ef599fa47eb
Henriique-Dev <vitorhenriquexaviier@gmail.com>
2026-03-10 20:08:17 -0300

    d03

--- desafios/d03/cad.php
+++ desafios/d03/cad.php
@@ -10,15 +10,20 @@
     <section>
         <h1>Conversor de Moedas v1.0</h1>
         <?php
-            $nmr = $_GET["nmr"];
-            $res = $nmr * 5.13;
-            $res = number_format($res , 2, ",", ".");
+            $cotacao = $_GET["nmr"] ?? 0;
+            $res = $cotacao / 5.13;
+            // $res = number_format($res , 2, ",", ".");
 
-            echo "Seus R$ " . $nmr . " equivalem a <strong>US$ " . $res . "</strong><br><br>";
-            echo "* <strong>Cotação fixa de 5,13</strong> informada diretamente no código.";
+            // echo "Seus R$ " . $cotacao . " equivalem a <strong>US$ " . $res . "</strong><br><br>";
+            // echo "* <strong>Cotação fixa de 5,13</strong> informada diretamente no código.";
+
+            $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
+
+            echo "<p>Seus " . numfmt_format_currency($padrao, $cotacao, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $res, "USD") . "</strong></p>";
+
         ?>
         <br><br>
-        <button onclick="history.back()">Voltar</button>
+        <button onclick="javascript:window.location.href='index.html'">Voltar</button>
 
     </section>
 </body>
